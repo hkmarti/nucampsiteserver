3824c878661104c0fc27c90ba63ea01da943afeb
Heather Martin <hkoto328@gmail.com>
2021-11-15 17:52:49 -0500

    Basic Authentication

--- app.js
+++ app.js
@@ -34,10 +34,46 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Order in which server runs//
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Set up authentication before accessing server//
+function auth(req, res, next){
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  
+  //if authorization header is null, send back error.//
+  if (!authHeader) {
+    const err = new Error('You are not authenticated.');
+    //Requests authentication from client. Authentication method is basic.// 
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  //Separates admin:password and puts it into an array//
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0]; //Sets first item in auth array ['admin', 'password'] to user.
+  const pass = auth[1]; //Sets second item in auth array to pass.
+
+  //If admin and password are 'admin' and 'password', then grant access to server.//
+  //Otherwise, request authentication again with 401 error.//
+  if (user === 'admin' && pass === 'password') {
+      return next(); //authorized
+  } else {
+      const err = new Error('You are not authenticated.');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+}
+
+//Calls auth function (above)//
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Set up appropiate path for each router//
