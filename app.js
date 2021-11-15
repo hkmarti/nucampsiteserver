126ac569e6d1fbcc417c281bce4f578c92b3b807
Heather Martin <hkoto328@gmail.com>
2021-11-15 18:27:44 -0500

    Cookies

--- app.js
+++ app.js
@@ -38,36 +38,54 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//Gives cookieParser a secret key//
+app.use(cookieParser('12345-67890-09876-54321'));
 
 //Set up authentication before accessing server//
 function auth(req, res, next){
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  
-  //if authorization header is null, send back error.//
-  if (!authHeader) {
-    const err = new Error('You are not authenticated.');
-    //Requests authentication from client. Authentication method is basic.// 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
+  //if cookie not properly signed...//
+  if(!req.signedCookies.user) {
+      const authHeader = req.headers.authorization;
+      
+      //if authorization header is null, send back error.//
+      if (!authHeader) {
+        const err = new Error('You are not authenticated.');
+        //Requests authentication from client. Authentication method is basic.// 
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+      }
 
-  //Separates admin:password and puts it into an array//
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0]; //Sets first item in auth array ['admin', 'password'] to user.
-  const pass = auth[1]; //Sets second item in auth array to pass.
+      //Separates admin:password and puts it into an array//
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const user = auth[0]; //Sets first item in auth array ['admin', 'password'] to user.
+      const pass = auth[1]; //Sets second item in auth array to pass.
 
-  //If admin and password are 'admin' and 'password', then grant access to server.//
-  //Otherwise, request authentication again with 401 error.//
-  if (user === 'admin' && pass === 'password') {
-      return next(); //authorized
+      //If admin and password are 'admin' and 'password', then grant access to server.//
+      //Otherwise, request authentication again with 401 error.//
+      if (user === 'admin' && pass === 'password') {
+          //Signs cookie.//
+          // {signed: true} tells Express to use secret key from cookieParser to create a signed cookie //
+          //sets cookie name to 'user'//
+          res.cookie('user','admin', {signed:true});
+          return next(); //authorized
+      } else {
+          const err = new Error('You are not authenticated.');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
   } else {
-      const err = new Error('You are not authenticated.');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
+      //if cookie is signed, checks to see if value is 'admin'//
+      if (req.signedCookies.user === 'admin') {
+        //if true, passes client onto the next middleware function//
+        return next();
+      } else {
+        //if value is not admin, sends 401 error//
+        const err = new Error('You are not authenticated.');
+        err.status = 401;
+        return next(err);
+      }
   }
 }
 
